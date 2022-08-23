c61563eae68b18007ee19f3ee45c6ae08d11a47c
fabiocuri <fcuri91@gmail.com>
2022-08-23 21:47:21 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,17 +20,9 @@ SUPER_RESOLUTION_FOLDER="../../debruits-images/sequence1/cropped"
 TO_BE_CROPPED_FOLDER="../../debruits-images/sequence1"
 
 if [ "$1" = "create" ]; then
- $PIP_VERSION install virtualenv
- virtualenv debruitssenv
- source debruitssenv/bin/activate
- $PIP_VERSION install -r requirements.txt
- cp -t debruitssenv ./src/*
- deactivate
+ $PIP_VERSION install -r /content/debruits/requirements.txt
 elif [[ "$1" = "preprocess" ]]; then
- cd debruitssenv
- source bin/activate
- $PYTHON_VERSION $PREPROCESS_PY $BRIGHTNESS $CONTRAST $BLUR $SATURATION
- deactivate
+ $PYTHON_VERSION /content/debruits/src/$PREPROCESS_PY $BRIGHTNESS $CONTRAST $BLUR $SATURATION
 elif [[ "$1" = "train" ]]; then
  cd debruitssenv
  source bin/activate
