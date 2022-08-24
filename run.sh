4d1ff21e2c409597bdbe5d50975628facb6aa850
fabiocuri <fcuri91@gmail.com>
2022-08-25 00:08:33 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,8 +16,8 @@ RDN_PY="rdn.py"
 IMAGE_MODEL_PY="imagemodel.py"
 IMAGE_PROCESSING_PY="image_processing.py"
 CROP_PY="crop.py"
-SUPER_RESOLUTION_FOLDER="../../debruits-images/sequence1/cropped"
-TO_BE_CROPPED_FOLDER="../../debruits-images/sequence1"
+SUPER_RESOLUTION_FOLDER="/content/drive/MyDrive/plots2/cropped"
+TO_BE_CROPPED_FOLDER="/content/drive/MyDrive/plots2"
 
 if [ "$1" = "create" ]; then
  $PIP_VERSION install -r /content/debruits/requirements.txt
@@ -26,18 +26,9 @@ elif [[ "$1" = "preprocess" ]]; then
 elif [[ "$1" = "train" ]]; then
  $PYTHON_VERSION /content/debruits/src/$TRAIN_PY $N_EPOCHS
 elif [[ "$1" = "inference" ]]; then
- cd debruitssenv
- source bin/activate
- $PYTHON_VERSION $INFERENCE_PY
- deactivate
+ $PYTHON_VERSION /content/debruits/src/$INFERENCE_PY
 elif [[ "$1" = "super_resolution" ]]; then
- cd debruitssenv
- source bin/activate
- $PYTHON_VERSION $SUPER_RESOLUTION_PY $N_SUPER_RESOLUTION $SUPER_RESOLUTION_FOLDER
- deactivate
+ $PYTHON_VERSION /content/debruits/src/$SUPER_RESOLUTION_PY $N_SUPER_RESOLUTION $SUPER_RESOLUTION_FOLDER
 elif [[ "$1" = "crop" ]]; then
- cd debruitssenv
- source bin/activate
- $PYTHON_VERSION $CROP_PY $TO_BE_CROPPED_FOLDER
- deactivate
+ $PYTHON_VERSION /content/debruits/src/$CROP_PY $TO_BE_CROPPED_FOLDER
 fi
