7b55805b60d065223078ae4b2fba2dab75138537
fabiocuri <fcuri91@gmail.com>
2022-08-25 18:36:03 +0100

    Update handlers.py

--- src/handlers.py
+++ src/handlers.py
@@ -146,7 +146,7 @@ class ImageClass:
 
         Path(output_path).mkdir(parents=True, exist_ok=True)
 
-        cv2.imwrite(f"{output_path}/{self.image_name}.jpg", scale * self.image)
+        cv2.imwrite(f"{output_path}/{self.image_name}.png", scale * self.image)
         print(self.image.shape)
 
     def imshow(self):
