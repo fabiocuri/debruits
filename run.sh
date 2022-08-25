5973a022f64ec6f84046cef5a09762c5056ff4f0
fabiocuri <fcuri91@gmail.com>
2022-08-25 17:53:04 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-N_EPOCHS=1
+N_EPOCHS=50
 N_SUPER_RESOLUTION=2
 BRIGHTNESS=10
 CONTRAST=1
@@ -7,8 +7,6 @@ BLUR=10
 SATURATION=3
 PYTHON_VERSION=python3
 PIP_VERSION=pip3
-SUPER_RESOLUTION_FOLDER="/content/drive/MyDrive/plots2/cropped"
-TO_BE_CROPPED_FOLDER="/content/drive/MyDrive/plots2"
 
 if [ "$1" = "create" ]; then
  $PIP_VERSION install -r /content/debruits/requirements.txt
@@ -20,8 +18,8 @@ elif [[ "$1" = "train" ]]; then
 elif [[ "$1" = "inference" ]]; then
  $PYTHON_VERSION /content/debruits/src/preprocess.py test $BRIGHTNESS $CONTRAST $BLUR $SATURATION
  $PYTHON_VERSION /content/debruits/src/inference.py
-elif [[ "$1" = "super_resolution" ]]; then
- $PYTHON_VERSION /content/debruits/src/super_resolution.py $N_SUPER_RESOLUTION $SUPER_RESOLUTION_FOLDER
 elif [[ "$1" = "crop" ]]; then
- $PYTHON_VERSION /content/debruits/src/crop.py $TO_BE_CROPPED_FOLDER
+ $PYTHON_VERSION /content/debruits/src/crop.py $2
+elif [[ "$1" = "super_resolution" ]]; then
+ $PYTHON_VERSION /content/debruits/src/super_resolution.py $2 $N_SUPER_RESOLUTION $SUPER_RESOLUTION_FOLDER
 fi
