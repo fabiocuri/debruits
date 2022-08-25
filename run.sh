6b02409b5071dc4fac55e67036805865b186bc88
fabiocuri <fcuri91@gmail.com>
2022-08-25 13:30:03 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-N_EPOCHS=200
+N_EPOCHS=1
 N_SUPER_RESOLUTION=2
 BRIGHTNESS=10
 CONTRAST=1
@@ -7,30 +7,21 @@ BLUR=10
 SATURATION=3
 PYTHON_VERSION=python3
 PIP_VERSION=pip3
-PREPROCESS_PY="preprocess.py"
-HANDLERS_PY="handlers.py"
-TRAIN_PY="train.py"
-INFERENCE_PY="inference.py"
-SUPER_RESOLUTION_PY="super_resolution.py"
-RDN_PY="rdn.py"
-IMAGE_MODEL_PY="imagemodel.py"
-IMAGE_PROCESSING_PY="image_processing.py"
-CROP_PY="crop.py"
 SUPER_RESOLUTION_FOLDER="/content/drive/MyDrive/plots2/cropped"
 TO_BE_CROPPED_FOLDER="/content/drive/MyDrive/plots2"
 
 if [ "$1" = "create" ]; then
  $PIP_VERSION install -r /content/debruits/requirements.txt
 elif [[ "$1" = "preprocess" ]]; then
- $PYTHON_VERSION /content/debruits/src/$PREPROCESS_PY train $BRIGHTNESS $CONTRAST $BLUR $SATURATION
- $PYTHON_VERSION /content/debruits/src/$PREPROCESS_PY val $BRIGHTNESS $CONTRAST $BLUR $SATURATION
+ $PYTHON_VERSION /content/debruits/src/preprocess.py train $BRIGHTNESS $CONTRAST $BLUR $SATURATION
+ $PYTHON_VERSION /content/debruits/src/preprocess.py val $BRIGHTNESS $CONTRAST $BLUR $SATURATION
 elif [[ "$1" = "train" ]]; then
- $PYTHON_VERSION /content/debruits/src/$TRAIN_PY $N_EPOCHS
+ $PYTHON_VERSION /content/debruits/src/train.py $1 $N_EPOCHS
 elif [[ "$1" = "inference" ]]; then
- $PYTHON_VERSION /content/debruits/src/$PREPROCESS_PY test $BRIGHTNESS $CONTRAST $BLUR $SATURATION
- $PYTHON_VERSION /content/debruits/src/$INFERENCE_PY
+ $PYTHON_VERSION /content/debruits/src/preprocess.py test $BRIGHTNESS $CONTRAST $BLUR $SATURATION
+ $PYTHON_VERSION /content/debruits/src/inference.py
 elif [[ "$1" = "super_resolution" ]]; then
- $PYTHON_VERSION /content/debruits/src/$SUPER_RESOLUTION_PY $N_SUPER_RESOLUTION $SUPER_RESOLUTION_FOLDER
+ $PYTHON_VERSION /content/debruits/src/super_resolution.py $N_SUPER_RESOLUTION $SUPER_RESOLUTION_FOLDER
 elif [[ "$1" = "crop" ]]; then
- $PYTHON_VERSION /content/debruits/src/$CROP_PY $TO_BE_CROPPED_FOLDER
+ $PYTHON_VERSION /content/debruits/src/crop.py $TO_BE_CROPPED_FOLDER
 fi
