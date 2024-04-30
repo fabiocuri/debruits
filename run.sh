22f1625d942d8220111965ed9a4b9b453d71d3e7
fabiocuri <fcuri91@gmail.com>
2024-05-01 00:37:22 +0100

    automated mongodb

--- run.sh
+++ run.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 minikube start
+
+# Clean namespace
 kubectl delete all --all -n debruits
 
-export MONGODB_USERNAME="debruits"
-export MONGODB_USERNAME_BASE64=echo -n $MONGODB_USERNAME | base64
-export MONGODB_PASSWORD="debruits"
-export MONGODB_PASSWORD_BASE64=echo -n $MONGODB_PASSWORD | base64
+# Install infrastructure using Helm templates
+helm install debruits-kubernetes ./debruits-kubernetes
 
-kubectl apply -f ./kubernetes/mongodb-configmap.yaml
-kubectl apply -f ./kubernetes/mongodb-secrets.yaml
-kubectl apply -f ./kubernetes/mongodb.yaml
-kubectl apply -f ./kubernetes/mongodb-express.yaml
-kubectl port-forward mongodb-7487f5894b-zm4hk 27017:27017 &
-kubectl port-forward mongo-express-86cf48bd49-shnxn 8081:8081 &
+# Clear ports and expose MongoDB and Mongo Express
+kill -9 $(lsof -t -i:8081)
+kill -9 $(lsof -t -i:27017)
+
+MONGODB_POD=$(kubectl get pods -l app=mongodb -o jsonpath='{.items[0].metadata.name}')
+MONGO_EXPRESS_POD=$(kubectl get pods -l app=mongo-express -o jsonpath='{.items[0].metadata.name}')
+kubectl port-forward $MONGODB_POD 27017:27017 &
+kubectl port-forward $MONGO_EXPRESS_POD 8081:8081 &
 
 sudo rm -rf venv
 sudo apt install python3.10-venv
