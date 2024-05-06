bde35d57de867dfb52fa538267aabf0efe46e748
fabiocuri <fcuri91@gmail.com>
2024-05-07 00:46:50 +0100

    refactor jenkinsfile

--- configure.sh
+++ configure.sh
@@ -37,7 +37,7 @@ kubectl create namespace jenkins
 kubens jenkins
 helm repo add jenkins https://charts.jenkins.io
 helm repo update
-helm install jenkins jenkins/jenkins
+helm install jenkins jenkins/jenkins -f ./kubernetes/jenkins.yaml
 kubectl apply -f ./kubernetes/jenkins-token.yaml
 echo "-----------BEGINNING TOKEN-----------"
 kubectl describe secret $(kubectl describe serviceaccount jenkins | grep token | awk '{print $2}')
